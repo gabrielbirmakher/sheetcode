86aaa3b1b7c163caa81b26f4d86065593e58f6b0
Gabriel Birmakher <gabriel.birmakher@gmail.com>
2024-02-25 16:22:53 -0500

    updates

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
+#include <iostream>
 #include "../sqlite3/sqlite3.h"
-#include "setup.hpp"
+#include "../include/setup.hpp"
 
 int main(){
-    
+    /*
+        Opens activity_log_file
+    */
+    FILE *activity_log_file;
+    activity_log_file = fopen("activityLog.txt", "w");
 
+    int test = setup();
+    if(test == 0){
+        fprintf(activity_log_file, "Cannot continue with operation, setup was unsuccessful\n");
+        return 0;
+    }
 
     return 0;
 }
--- src/setup.cpp
+++ src/setup.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "../sqlite3/sqlite3.h"
-#include "setup.hpp"
+#include "../include/setup.hpp"
 
 int setup(void){
     /*
@@ -29,11 +29,12 @@ int setup(void){
     /*
         Attempts to create user table;
     */
-    query = "CREATE TABLE user(id INTEGER, pass TEXT, total INTEGER, easy INTEGER, medium INTEGER, hard INTEGER, recentid INTEGER);";
-    rc = sqlite3_exec(db, query, 0, 0, &errMsg);
+    query = "CREATE TABLE IF NOT EXISTS user(id INTEGER, pass TEXT, total INTEGER, easy INTEGER, medium INTEGER, hard INTEGER, recentid INTEGER);";
+    rc = sqlite3_exec(db, query, nullptr, nullptr, &errMsg);
     if(rc != SQLITE_OK){
         fprintf(activity_log_file, "Failed to create user table in database: %s\n", errMsg);
         sqlite3_free(errMsg); 
+        sqlite3_close(db);
         return 0;
     } else {
         fprintf(activity_log_file, "Made users table in database\n");
@@ -41,15 +42,54 @@ int setup(void){
     /*
         Attempts to create leetcodes table;
     */
-    query = "CREATE TABLE leetcodes(id INTEGER, title TEXT, status INTEGER);";
-    rc = sqlite3_exec(db, query, 0, 0, &errMsg);
+    query = "CREATE TABLE IF NOT EXISTS leetcodes(id INTEGER, title TEXT, status INTEGER);";
+    rc = sqlite3_exec(db, query, nullptr, nullptr, &errMsg);
     if(rc != SQLITE_OK){
         fprintf(activity_log_file, "Failed to create leetcodes table in database: %s\n", errMsg);
         sqlite3_free(errMsg); 
+        sqlite3_close(db);
         return 0;
     } else {
         fprintf(activity_log_file, "Made leetcodes table in database\n"); 
     }
+    /*
+        Checks if the user table is empty, if so, create default user
+    */
+    query = "SELECT COUNT(*) FROM user";
+    sqlite3_stmt* stmt;
+    rc = sqlite3_prepare_v2(db, query, -1, &stmt, NULL);
+    if(rc != SQLITE_OK){
+        fprintf(activity_log_file, "Failed to prepare statement: %s\n", sqlite3_errmsg(db));
+        sqlite3_close(db);
+        return 0;
+    }
+    rc = sqlite3_step(stmt);
+    if(rc != SQLITE_ROW){
+        fprintf(activity_log_file, "sqlite3_step failed: %s\n", sqlite3_errmsg(db));
+        fprintf(activity_log_file, "sqlite3_step result code: %d\n", rc);
+        sqlite3_finalize(stmt);
+        sqlite3_close(db);
+        return 0;
+    }
+    int count = sqlite3_column_int(stmt, 0);
+    if(count == 0){
+        query = "INSERT INTO user(id, pass, total, easy, medium, hard, recentid) VALUES (1, 'temp', 0, 0, 0, 0, 0)";
+        rc = sqlite3_exec(db, query, nullptr, nullptr, &errMsg);
+        if(rc != SQLITE_OK){
+            fprintf(activity_log_file, "Failed to insert user into database: %s\n", errMsg);
+            sqlite3_free(errMsg);
+            sqlite3_finalize(stmt);
+            sqlite3_close(db);
+            return 0;
+        } else {
+            fprintf(activity_log_file, "User created\n");
+        }
+    } else if(count == 1){
+        fprintf(activity_log_file, "User already exists\n");
+    } else {
+        fprintf(activity_log_file, "Problem with user table\n");
+    }
+    sqlite3_finalize(stmt);
     /*
         Close databse and activityLog.txt
     */
