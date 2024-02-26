3ffa13ca51b2cc833697b33dc28ed66db81522f1
Gabriel Birmakher <gabriel.birmakher@gmail.com>
2024-02-26 18:50:09 -0500

    updates

---
+++ include/user.hpp
@@ -0,0 +1,20 @@
+#ifndef USER_HPP_
+#define USER_HPP_
+
+int getNumTotal(int userId);
+
+void updateNumTotal(int userId);
+
+int getNumEasy(int userId);
+
+void updateNumEasy(int userId);
+
+int getNumMedium(int userId);
+
+void updateNumMedium(int userId);
+
+int getNumHard(int userId);
+
+void updateNumHard(int userId);
+
+#endif
